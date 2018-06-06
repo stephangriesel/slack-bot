16be0efc8093db5da5d17d3d21a0814d494190fb
Stephan Griesel <sgriesel@gmail.com>
2018-06-06 20:46:02 +0200

    add that really jokes

--- index.js
+++ index.js
@@ -32,50 +32,77 @@ bot.on("message", data => {
 
 // response to data
 function handleMessage(message) {
-  if (message.includes(' chucknorris')) {
+  if (message.includes(" chucknorris")) {
     chuckJoke();
-  } else if(message.includes(' yomama')) {
-      yoMamaJoke();
-  } else if(message.includes(' random')) {
-      randomJoke();
+  } else if (message.includes(" yomama")) {
+    yoMamaJoke();
+  } else if (message.includes(" thatreally")) {
+    thatReally();
+  } else if (message.includes(" random")) {
+    randomJoke();
+  } else if (message.includes(" help")) {
+    runHelp();
   }
 }
 
 // tell chuck joke
 function chuckJoke() {
-  axios.get('http://api.icndb.com/jokes/random').then(res => {
+  axios.get("http://api.icndb.com/jokes/random").then(res => {
     const joke = res.data.value.joke;
 
     const params = {
-        icon_emoji: ":laughing:"
-      };
-    
-      bot.postMessageToChannel('general', `Chuck Norris: ${joke}`, params);
+      icon_emoji: ":laughing:"
+    };
 
+    bot.postMessageToChannel("general", `Chuck Norris: ${joke}`, params);
   });
 }
 
 // yomama joke
 function yoMamaJoke() {
-    axios.get('http://api.yomomma.info').then(res => {
-      const joke = res.data.joke;
+  axios.get("http://api.yomomma.info").then(res => {
+    const joke = res.data.joke;
+
+    const params = {
+      icon_emoji: ":laughing:"
+    };
+
+    bot.postMessageToChannel("general", `Yo mamma: ${joke}`, params);
+  });
+}
+
+// tell a random joke
+function randomJoke() {
+  const rand = Math.floor(Math.random() * 2) + 1;
+  if (rand === 1) {
+    chuckJoke();
+  } else if (rand === 2) {
+    yoMamaJoke();
+  }
+}
+
+// that really
+function thatReally() {
+    axios.get("http://api.chew.pro/trbmb").then(res => {
+      const joke = res.data;
   
       const params = {
-          icon_emoji: ':laughing:'
-
-        };
-      
-        bot.postMessageToChannel('general', `Yo mamma: ${joke}`, params);
+        icon_emoji: ":laughing:"
+      };
   
+      bot.postMessageToChannel("general", `That Really: ${joke}`, params);
     });
   }
 
-  // tell a random joke
-  function randomJoke() {
-      const rand = Math.floor(Math.random() * 2) + 1;
-      if(rand === 1) {
-          chuckJoke();
-      } else if(rand === 2) {
-          yoMamaJoke();
-      }
-  }
+// help
+function runHelp() {
+  const params = {
+    icon_emoji: ":question:"
+  };
+
+  bot.postMessageToChannel(
+    "general",
+    `Help: Type @jokebot with 'chucknorris', 'yomama' or 'random' to get a reply with joke`,
+    params
+  );
+}
