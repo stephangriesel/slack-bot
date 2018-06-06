fc897c63807922d4f63fb91af4555fbf8428a5b2
Stephan Griesel <sgriesel@gmail.com>
2018-06-06 19:42:15 +0200

    fix const axios

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-const SlackBot = require('slackbots');
-const axious = require('axios');
+const SlackBot = require("slackbots");
+const axios = require("axios");
 
 const bot = new SlackBot({
-    token: 'xoxb-376558584390-374971627008-s5f9V4pCLiFA8BoBZRMJ6uAi',
-    name: 'slack-bot'
+  token: "xoxb-376558584390-374971627008-s5f9V4pCLiFA8BoBZRMJ6uAi",
+  name: "slack-bot"
 });
 
 // Start bot
 
-bot.on('start', () => {
-    const params = {
-        icon_emoji: ':smiley:' 
-    }
+bot.on("start", () => {
+  const params = {
+    icon_emoji: ":smiley:"
+  };
 
-    bot.postMessageToChannel('general', 'This is going to be funny...', params);
+  bot.postMessageToChannel("general", "This is going to be funny...", params);
 });
 
-
 // error handle
-bot.on('error', (err) => console.log(err));
-
+bot.on("error", err => console.log(err));
 
 // msg handle
 
-bot.on('message', (data) => {
-    if(data.type !== 'message') {
-        return;
-    }
+bot.on("message", data => {
+  if (data.type !== "message") {
+    return;
+  }
 
-    console.log(data);
-    //handleMessage(data.text);
+  // console.log(data);
+  handleMessage(data.text);
 });
 
-// repsonse to data 
+// response to data
 function handleMessage(message) {
-    if(message.includes(' chucknorris')) {
-        chuckJoke();
-    }
+  if (message.includes(" chucknorris")) {
+    chuckJoke();
+  }
 }
 
 // tell chuck joke
-/* function chuckJoke(){
-    axious.get('http://api.icndb.com/jokes/random')
-    .then(res -> {
-        const joke = res.data.value.joke;
-    })
-}*/
+function chuckJoke() {
+  axios.get('http://api.icndb.com/jokes/random').then(res => {
+    const joke = res.data.value.joke;
+
+    const params = {
+        icon_emoji: ":laughing:"
+      };
+    
+      bot.postMessageToChannel('general', `Chuck Norris: ${joke}`, params);
+
+  });
+}
